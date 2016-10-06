8872267b3ee0fbb02eda55ea5838eee65e692cd8
McLuft <McLuft@McLufts-MacBook-Air.local>
2016-10-06 17:01:58 +0200

    initial wrapper

--- Host.cu.h
+++ Host.cu.h
@@ -6,12 +6,14 @@
 
 template <class T>
 void arraySgm (unsigned int      d_size,
-               unsigned int d_size_hist,
+               unsigned int d_hist_size,
                unsigned int  block_size,
+               T               boundary,
                T*                  d_in,
                T*                 d_out) {
-  int num_blocks = block_size;
-  
+  int num_blocks = ceil(d_size / block_size);
+
+  mapKer<T><<<num_blocks, block_size>>>(d_size, d_hist_size, boundary, d_in, d_out);
 }
 
 #endif HOST_HIST
