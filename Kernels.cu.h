0adeeba596cbe440ac4ba745253e8e73090a388f
Christian Kjær Larsen <wkm839@a00332.science.domain>
2016-10-07 14:29:39 +0200

    Stuff

--- Kernels.cu.h
+++ Kernels.cu.h
@@ -2,6 +2,8 @@
 #define KERNELS_HIST
 #include <cuda_runtime.h>
 
+#define GPU_HIST_SIZE 8192
+
 template <class T>
 __global__ void mapKer(unsigned int  tot_size,
                        unsigned int hist_size,
@@ -13,6 +15,19 @@ __global__ void mapKer(unsigned int  tot_size,
     // ... implement f for index corresponding to indices.
     d_out[gid] = floor(d_in[gid]/boundary) * hist_size;
   }
+}
+
+__global__ void naiveHistKernel(unsigned int  tot_size,
+                                int*              inds,
+                                int*              hist) {
+
+  __shared__ int Hsh[8192];
+
+  const unsigned int gid = blockIdx.x * blockDim.x + threadIdx.x;
+  if (gid < tot_size) {
+    atomicAdd(&hist[inds[gid]], 1);
+  }
+
 }
 
 #endif KERNELS_HIST
