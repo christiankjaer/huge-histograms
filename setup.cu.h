f2886278dbd3190a926181c958094c1076b8cde1
FurryRogue <joachim.tilsted@gmail.com>
2016-10-27 15:35:21 +0200

    mere hardware paralisp.

--- setup.cu.h
+++ setup.cu.h
@@ -5,5 +5,6 @@
 #define CHUNK_SIZE      8192    // Chunk size for the GPU to work on, maximum 8192.
 #define RADIX_END_BIT      8    // Number of bit for 1 byte
 #define CUDA_BLOCK_SIZE  512    // Kernel blocksize
+#define HARDWARE_PARALLELISM 65536 // 2^16
 
 #endif //SETUP
